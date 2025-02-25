483e2d9ecd6d539df4ea515c8394fdada96d2486
Mylena Marques Ribeiro <ribeiro.mylena1@gmail.com>
2025-02-25 13:28:58 -0300

    Consumindo uma API externa fake a partir do db.json

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,24 +1,29 @@
 import Banner from "components/Banner";
-// import Cabecalho from "components/Cabecalho";
 import Card from "components/Card";
-// import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
 import styles from "./Inicio.module.css";
-import videos from "json/db.json";
+import { useEffect, useState } from "react";
+// import videos from "json/db.json";
 
 function  Inicio() {
+    const [videos, setVideos] = useState([]);
+    useEffect(() => {
+        fetch("https://my-json-server.typicode.com/MilyRibeiro/cinetag/videos")
+            .then(resposta => resposta.json())
+            .then(dados => {
+            setVideos(dados);
+        })
+    }, []);
+    
     return (
         <>
-            {/* <Cabecalho /> */}
             <Banner imagem="home" />
             <Titulo>
                 <h1>Um lugar para guardar seus vídeos e filmes!</h1>
             </Titulo>
-            {/* <Card id='1' titulo="Gato fofo" imagem='C:\Users\ribei\OneDrive\Imagens\muito fofo.webp' /> */}
             <section className={styles.container}>{videos.map((video) => {
                 return <Card {...video} key={video.id} />
             })}</section>
-            {/* <Rodape /> */}
         </>
      
     )
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,3 @@
-// import Cabecalho from "components/Cabecalho";
-// import Container from "components/Container";
-// import Rodape from "components/Rodape";
-// import FavoritosProvider from "contextos/Favoritos";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
 import NaoEncontrada from "pages/NaoEncontrada";
@@ -12,21 +8,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 function AppRoutes() {
     return (
         <BrowserRouter>
-            {/* <Cabecalho />
-            <Container>
-                <FavoritosProvider> */}
             <Routes>
                 <Route path="/" element={<PaginaBase />}>
-                            {/* <Route path="/" element={<Inicio />}></Route> */}
                     <Route index element={<Inicio />}></Route>
                     <Route path="favoritos" element={<Favoritos />}></Route>
                     <Route path=":id" element={<Player />}></Route>
                     <Route path="*" element={<NaoEncontrada />}></Route>
                 </Route>
             </Routes>
-                {/* </FavoritosProvider>
-            </Container>
-            <Rodape /> */}
         </BrowserRouter>
     )
 }
